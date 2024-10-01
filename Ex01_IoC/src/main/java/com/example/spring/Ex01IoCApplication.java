7d27e83b01a614acff381e43cb93eb16afc72a03
minwook <minukhan>
2024-10-01 10:46:09 +0900

    start

---
+++ Ex01_IoC/src/main/java/com/example/spring/Ex01IoCApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01IoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01IoCApplication.class, args);
+	}
+
+}
