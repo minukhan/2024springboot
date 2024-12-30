1c0e487032ede854315243ebcb1cef071875f594
minwook <minukhan>
2024-12-30 19:22:42 +0900

    Study Autoever SpringBoot

--- Ex14_Security/src/main/java/com/sample/spring/config/CustomSecurityConfig.java
+++ Ex14_Security/src/main/java/com/sample/spring/config/CustomSecurityConfig.java
@@ -29,7 +29,7 @@ import lombok.extern.log4j.Log4j2;
 @EnableMethodSecurity
 public class CustomSecurityConfig {
 
-	@Bean // test
+	@Bean // test123
 	public SecurityFilterChain filterchain(HttpSecurity http) throws Exception {
 		log.info("################### security config ######################");
 
